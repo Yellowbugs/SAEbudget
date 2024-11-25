aefeb474bc98ad237d4c0a64dab5861cd49bae3a
yellowbugs <jd.venice@icloud.com>
2024-11-25 05:26:52 -0500

    base login confirmation v2

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.get('/*', function (req,res) {
-    res.sendFile(req.url, {root: './public'});
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/dashboard', function (req, res) {
+    res.sendFile(path.join(__dirname,'public', 'dashboard.html'))
 });
+
+app.post('/login', urlencodedParser, function(req,res) {
+    if(req.body.username == "Josh" && req.body.password == "12345"){
+        res.sendStatus(200)
+    }
+    else{
+        res.sendStatus(404)
+    }
+});
+
+
 
 app.listen(PORT, () => console.log(`Our app is running on port ${PORT}`));
