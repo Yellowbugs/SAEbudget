d80030550ed342006f9c380af9fd63db0a45909d
yellowbugs <jd.venice@icloud.com>
2024-11-25 05:19:40 -0500

    added base login confirmation

---
+++ public/index.js
@@ -0,0 +1,14 @@
+function login(){
+    $.post('/login',{username: document.getElementById('username').value, password: document.getElementById('password').value})
+    .done(function(data) {
+        alert('Login successful!');
+        window.location.href = '/dashboard';
+    })
+    .fail(function(jqXHR) {
+        if (jqXHR.status === 404) {
+            alert('User not found');
+        } else {
+            alert('An error occurred: ' + jqXHR.status);
+        }
+    });
+}
