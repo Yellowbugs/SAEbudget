6a2a26a627c3eb6e36aaf0edb46ea1525227db54
yellowbugs <jd.venice@icloud.com>
2024-11-26 04:11:40 -0500

    DB connection

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const { Client } = require('pg')
+var conString = 'postgresql://saebudgetdb_user:cqqkgEHDgj0QBUa2zhgNzrOm0rfBANbr@dpg-ct2or1jv2p9s73b0b0gg-a/saebudgetdb'
+const client = new Client(conString)
 
 const app = express();
 const PORT = process.env.PORT || 10000;
@@ -14,9 +16,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/dashboard', function (req, res) {
     res.sendFile(path.join(__dirname,'public', 'dashboard.html'))
+
 });
 
+
 app.post('/login', urlencodedParser, function(req,res) {
+    client.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
     if(req.body.username == "Josh" && req.body.password == "12345"){
         res.sendStatus(200)
     }
